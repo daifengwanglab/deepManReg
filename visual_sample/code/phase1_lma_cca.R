20b89879752449bb1d6df6fade393d8cf5969da5
daifengwanglab <57010174+daifengwanglab@users.noreply.github.com>
2021-11-27 22:40:03 -0600

    Add files via upload

--- visual_sample/code/phase1_lma_cca.R
+++ visual_sample/code/phase1_lma_cca.R
@@ -11,18 +11,18 @@ gdata = read.csv('../data/expMat_filtered.csv',row.names = 1)
 label = read.csv('../data/label_visual.csv')
 
 #e-feature
-X = t(edata)
+X = apply(edata,1,scale)
 #g-feature
-Y= gdata
+Y = log(gdata+1)
 corr = Correspondence(matrix = cor(t(X),t(Y)))
 #corr=Correspondence(matrix=diag(nrow(X)))
 
 #lma
-NMA_res = ManiNetCluster(X,Y,nameX='Ephys',nameY='Expr',corr=corr,method = method[1],d=10L,k_NN=5L,k_medoids=5L)
-x = NMA_res[3:12]
+NMA_res = ManiNetCluster(X,Y,nameX='Ephys',nameY='Expr',corr=corr,method = method[1],d=3L,k_NN=5L,k_medoids=5L)
+x = NMA_res[3:5]
 write.csv(x,'../data/ma_latent.csv',row.names = F)
 
 #cca
-NMA_res = ManiNetCluster(X,Y,nameX='Ephys',nameY='Expr',corr=corr,method = method[2],d=10L,k_NN=5L,k_medoids=5L)
-x = NMA_res[3:12]
+NMA_res = ManiNetCluster(X,Y,nameX='Ephys',nameY='Expr',corr=corr,method = method[2],d=3L,k_NN=5L,k_medoids=5L)
+x = NMA_res[3:5]
 write.csv(x,'../data/cca_latent.csv',row.names = F)
